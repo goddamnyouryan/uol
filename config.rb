64dd045b18089cc46545b5bcc4e4245a9d17ff2f
Ryan MacInnes <ryan.macinnes@gmail.com>
2014-08-02 00:22:40 -0300

    Adding fonts.

--- config.rb
+++ config.rb
@@ -53,10 +53,9 @@
 require 'slim'
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
+set :fonts_dir, 'fonts'
 
 # Build-specific configuration
 configure :build do
